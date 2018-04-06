75c969b9d71fcd4ee82ca1c28131c9fbae193d6a
Parfait G <parfait.gasana@gmail.com>
2018-04-06 10:35:46 -0500

    Replaced vapply calls for replicate

--- RandomData.r
+++ RandomData.r
@@ -7,12 +7,12 @@ as.POSIXct(epoch_time, origin="1970-01-01")
 
 lapply(seq(10), function(i) {
   df <- data.frame(
-       ID = vapply(seq(50), function(i) sample(seq(15), 1, replace=TRUE), integer(1)),
-       GROUP = vapply(seq(50), function(i) sample(LETTERS, 1, replace=TRUE), character(1)),
-       DATE = as.POSIXct(vapply(seq(50), function(i) sample(epoch_time, 1, replace=TRUE), integer(1)), origin="1970-01-01"),
+       ID = replicate(50, sample(seq(15), 1, replace=TRUE)),
+       GROUP = replicate(50, sample(LETTERS, 1, replace=TRUE)),
+       DATE = as.POSIXct(replicate(50, sample(epoch_time, 1, replace=TRUE)), origin="1970-01-01"),
        NUM1 = rnorm(50)*100,
        NUM2 = rnorm(50),
-       BOOL = vapply(seq(50), function(i) sample(c("TRUE", "FALSE"), 1, replace=TRUE), character(1))
+       BOOL = replicate(50, sample(c("TRUE", "FALSE"), 1, replace=TRUE))
   )
  write.csv(df, paste0("RandomData_R_",i,".csv"))
  return(df)
